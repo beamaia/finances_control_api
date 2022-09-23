ceddb519b2a7d7c9b964020bc5b39f97e4c24b48
Bea Maia <beamaia.work@gmail.com>
2022-09-23 00:44:59 -0300

    :sparkles: adds init files

---
+++ app/__init__.py
@@ -0,0 +1,1 @@
+from app.api_v1 import app
---
+++ app/api_v1/__init__.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from enum import Enum
+
+app = FastAPI()
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/debit_card/{id}")
+async def read_item(id):
+    return {"id": id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
