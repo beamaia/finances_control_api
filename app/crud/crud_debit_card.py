b061a3ff03a91380c199639b4c1986605515ec2e
beamaia <beamaia.work@gmail.com>
2022-10-06 17:53:09 -0300

    :recycle: considers base update format

---
+++ app/crud/crud_debit_card.py
@@ -0,0 +1,15 @@
+from typing import List
+
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy.orm import Session
+
+from app.crud.base import CRUDBase
+from app.models.schemas.debit_card import DebitCardCreate, DebitCardUpdate
+from app.models.database.debit_card import DebitCard
+
+
+class CrudDebitCard(CRUDBase[DebitCard, DebitCardCreate, DebitCardUpdate]):
+    pass
+
+
+debit_card = CrudDebitCard(DebitCard)
