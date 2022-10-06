1b5586ea9df7df7eaf4685bee67e9ade9ec949e4
Bea Maia <beamaia.work@gmail.com>
2022-10-06 13:26:15 -0300

    :sparkles: creates req schemas

--- app/models/schemas/debit_card.py
+++ app/models/schemas/debit_card.py
@@ -2,10 +2,44 @@ from typing import Optional
 
 from pydantic import BaseModel
 
-class DebitCard(BaseModel):
+# Shared properties
+class DebitCardBase(BaseModel):
+    name: str = None
+    amount: float = 0
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+# Properties to receive on debit card creation
+class DebitCardCreate(DebitCardBase):
+    name: str
+    amount: float = 0
+
+
+# Properties to receive on debit card update
+class DebitCardUpdate(DebitCardBase):
+    name: str
+    amount: float
+
+
+# Properties shared by models stored in DB
+class DebitCardInDBBase(DebitCardBase):
     name: str
     amount: float
     id: int
 
     class Config:
         orm_mode = True
+
+
+# Properties to return to client
+class DebitCard(DebitCardInDBBase):
+    name: str
+    amount: float
+
+
+# Properties properties stored in DB
+class DebitCardInDB(DebitCardInDBBase):
+    pass
