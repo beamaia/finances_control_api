6a618a50163a9e866cd8b885d24e7900ae4d7b21
Bea Maia <beamaia.work@gmail.com>
2022-10-28 07:45:14 -0300

    :sparkles: adds functions to init

---
+++ app/models/schemas/__init__.py
@@ -0,0 +1,3 @@
+from debit_extract import DebitExtract, DebitExtractCreate, DebitExtractUpdate
+from debit_card import DebitCard, DebitCardCreate, DebitCardUpdate
+from credit_card import CreditCard, CreditCardCreate, CreditCardUpdate
