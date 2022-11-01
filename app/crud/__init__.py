e4317f75a454a855c15183ed4fe93d1ecaf533fa
Bea Maia <beamaia.work@gmail.com>
2022-10-31 21:40:31 -0300

    :sparkles: adds credit card, debit extract and category

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -1,1 +1,6 @@
 from app.crud.crud_debit_card import debit_card
+from app.crud.crud_credit_card import credit_card
+
+from app.crud.crud_debit_extract import debit_extract
+
+from app.crud.crud_category import category
