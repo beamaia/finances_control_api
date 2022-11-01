e77d7c074b9fa34e52df3eb81a099f2de414111e
Bea Maia <beamaia.work@gmail.com>
2022-10-31 21:40:43 -0300

    :sparkles: adds category

--- app/models/schemas/__init__.py
+++ app/models/schemas/__init__.py
@@ -1,3 +1,4 @@
 from app.models.schemas.debit_extract import DebitExtract, DebitExtractCreate, DebitExtractUpdate
 from app.models.schemas.debit_card import DebitCard, DebitCardCreate, DebitCardUpdate
 from app.models.schemas.credit_card import CreditCard, CreditCardCreate, CreditCardUpdate
+from app.models.schemas.category import Category, CategoryCreate, CategoryUpdate
