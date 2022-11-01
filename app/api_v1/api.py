2512a0e56bd872f66aef185b9d5b29f0d1f5b435
Bea Maia <beamaia.work@gmail.com>
2022-10-31 21:34:03 -0300

    :sparkles: adds category, credit card and (WIP) debit extract routes

--- app/api_v1/api.py
+++ app/api_v1/api.py
@@ -1,6 +1,13 @@
 from fastapi import APIRouter
 
 from app.api_v1.endpoints import debit_card
+from app.api_v1.endpoints import debit_extract
+from app.api_v1.endpoints import category
+from app.api_v1.endpoints import credit_card
 
 api_router = APIRouter()
+
+api_router.include_router(category.router)
 api_router.include_router(debit_card.router)
+api_router.include_router(debit_extract.router)
+api_router.include_router(credit_card.router)
